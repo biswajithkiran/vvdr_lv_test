4d641e43a104bc91ac9ec13e60c328cc31c2ecd9
Biswajith <biswajithkr@gmail.com>
2024-01-23 23:09:08 +0530

    bootstrap folder

---
+++ bootstrap/cache/departments_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Departments\\Providers\\DepartmentsServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Departments\\Providers\\DepartmentsServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
